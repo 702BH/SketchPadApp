39381e7291f5ff329b6d68200d5cf988cb522259
702BH <65957905+702BH@users.noreply.github.com>
2023-10-03 18:14:29 +0300

    Update Feature extraction.R

--- sketchpadapp_v1/Data/Feature extraction.R
+++ sketchpadapp_v1/Data/Feature extraction.R
@@ -33,5 +33,46 @@ get_point_count <- function(path){
 
 
 
+csv_file <- "samples.csv"
+
+df <- read.csv(csv_file)
+
+head(df)
 
 
+input_folder <- "dataset/json"
+
+json_files <- list.files(input_folder, pattern = "*.json", 
+                         full.names = TRUE)
+
+combined_data <- list()
+
+extract_values <- function(row){
+  
+  id <- row["id"]
+  label <- row["drawing"]
+  student_name <- row["student"]
+  student_id <- row["session"]
+  return(list(ID = id, Label = label, Name = student_name,
+              student_id = student_id))
+  
+}
+
+
+
+for(sample in df[1,]){
+  json_file_name <- paste0(sample["id"], ".json")
+  
+  if(json_file_name %in% basename(json_files)){
+    
+    json_data <- fromJSON(paste0(input_folder, "/", json_file_name))
+    
+    t <- get_path_count(json_data)
+    print(t)
+
+  }else{
+    cat(paste("JSON file", json_file_name, "not found.\n"))
+  }
+  
+  
+}
